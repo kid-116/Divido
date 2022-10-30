0757a7273cf05fa807050703b72ee0f4f3dd0c96
Mehul Todi <75692643+kid-116@users.noreply.github.com>
2022-10-31 00:01:36 +0530

    Fix problems with site

---
+++ site/components/CarCard.js
@@ -0,0 +1,30 @@
+const loremPicsum = require("lorem-picsum");
+
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+};
+
+export default function CarCard(props) {
+    const API_KEY = 'd0a74699e3ef4efcb52d4d4f41ca388b'
+    
+    function getImage() {
+        return `https://maps.geoapify.com/v1/staticmap?width=500&height=400&marker=lonlat:${props.car['longitude']},${props.car['latitude']}&apiKey=${API_KEY}`;
+    }
+
+    return (
+        <div class="option" id={`car-${props.car['id']}`} style={{ '--optionBackground': `url(${getImage()}` }} onClick={() => { props.handleClick(props.car['id']) }}>
+            <div class="shadow"></div>
+            <div class="label">
+                <div class="icon" onClick={() => {
+                    window.open('https://www.google.com/maps/search/?api=1&query=' + props.car.latitude + ',' + props.car.longitude);
+                }}>
+                    <i class="fa-solid fa-car"></i>
+                </div>
+                <div class="info">
+                    <div id={`car-model-${props.car['id']}`}>{props.car['model']}</div>
+                    <div id={`car-sub-${props.car['id']}`}>{`${props.car['latitude']},${props.car['longitude']}`}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ site/pages/index.js
@@ -0,0 +1,48 @@
+import Head from 'next/head'
+import { useEffect, useState } from 'react';
+import CarCard from '../components/CarCard'
+
+export default function Home() {
+
+  const [cars, setCars] = useState([]);
+  function getCars() {
+    fetch('http://divido.mehultodi.repl.co/car')
+      .then((response) => response.json())
+      .then((cars) => {
+        setCars(cars);
+        handleClick(1);
+      })
+  }
+
+  useEffect(() => {
+    getCars();
+  }, []);
+
+  function handleClick(id) {
+    console.log(id);
+    for(const option of document.getElementsByClassName('option')) {
+      if(option.id == `car-${id}`) {
+        option.className = 'option active'
+      }
+      else {
+        option.className = 'option'
+      }
+    }
+  }
+
+  return (
+    <div className="container">
+      <Head>
+        <title>Divido | Car Tracking</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" />
+        <link rel="stylesheet" href="https://static.fontawesome.com/css/fontawesome-app.css" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://kit.fontawesome.com/b19048fdd6.js" crossorigin="anonymous"></script>
+      </Head>
+      <div class="options">
+        {cars.map(car => <CarCard car={car} handleClick={handleClick} key={`car-${car['id']}`} />)}
+      </div>
+    </div>
+  )
+}
